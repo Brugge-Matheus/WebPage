ecd9a4f6d5e7342052147ab7113eb19f11dace47
Brugge-Matheus <128723144+Brugge-Matheus@users.noreply.github.com>
2024-06-30 22:04:12 -0300

    first changes

---
+++ app/views/master.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+    <h2>Dynamic PHP - <?=$name?></h2>
+
+    <div class="container">
+        <?php require VIEW_DIR.$view?>
+    </div>
+
+</body>
+
+</html>
